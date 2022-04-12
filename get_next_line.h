a7e4d349efdbda986c3da357e0f2aed4a1f40292
Denny Cerritos portillo <dcerrito@c3r13s5.42madrid.com>
2022-04-12 04:42:22 +0200

    chores: add barenone setup

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/12 04:36:45 by dcerrito          #+#    #+#             */
+/*   Updated: 2022/04/12 04:39:19 by dcerrito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
