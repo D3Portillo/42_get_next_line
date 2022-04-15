8b5e7c68dded6b9147c1cd8014fd83e559bbbd1c
Denny Cerritos portillo <dcerrito@c3r14s2.42madrid.com>
2022-04-15 02:46:32 +0200

    enh(get_next_line.h): include libft function definitions

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 04:36:45 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/04/12 04:39:19 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/04/15 02:12:12 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,14 @@
 #  define BUFFER_SIZE 42
 # endif
 
+# include <unistd.h>
+# include <stdlib.h>
+
 char	*get_next_line(int fd);
+char	*ft_substr(char const *str, unsigned int start, size_t size);
+int		ft_strlen(char *str);
+void	ft_strcpy(char *dest, char *src, int limit);
+void	ft_strlcat(char *dest, int dest_size, char *src, int limit);
+char	*ft_strdup(char *source);
 
 #endif
