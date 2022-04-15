7d9ba335b1b898c500f84844660adbb0603f5ec9
Denny Cerritos portillo <dcerrito@c3r14s2.42madrid.com>
2022-04-15 02:48:51 +0200

    enh(get_next_line_utils.c): add libft utils implementation

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/13 04:22:28 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/04/14 07:01:21 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/04/15 02:13:00 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,29 +39,23 @@ char	*ft_substr(char const *src, unsigned int start, size_t size)
 	return (result);
 }
 
-void	ft_strlcpy(char *dest, char *src, int limit)
+void	ft_strcpy(char *dest, char *src, int limit)
 {	
 	int	i;
 
 	i = -1;
-	while (++i, src && src[i] && (i + 1) < limit)
+	while (++i, src && src[i] && i < limit)
 		dest[i] = src[i];
-	if (src && limit > 0)
-		dest[i] = '\0';
 }
 
-void	ft_strlcat(char *dest, char *src, int limit)
+void	ft_strlcat(char *dest, int dest_size, char *src, int limit)
 {
-	int	i;
 	int	j;
-	int	dest_size;
 
 	j = 0;
-	i = ((dest_size = ft_strlen(dest)));
-	while (src[j] && (i + 1) < limit)
-		dest[i++] = src[j++];
-	if (limit > 0 && dest_size <= limit)
-		dest[i] = '\0';
+	while (src[j] && (dest_size + 1) < limit)
+		dest[dest_size++] = src[j++];
+	dest[dest_size] = '\0';
 }
 
 char	*ft_strdup(char *source)
@@ -69,10 +63,11 @@ char	*ft_strdup(char *source)
 	int		size;
 	char	*dest;
 
-	size = 1 + ft_strlen(source);
-	dest = malloc(size);
+	size = ft_strlen(source);
+	dest = malloc(size + 1);
 	if (!dest)
 		return (NULL);
-	ft_strlcpy(dest, source, size);
+	ft_strcpy(dest, source, size);
+	dest[size] = '\0';
 	return (dest);
 }
